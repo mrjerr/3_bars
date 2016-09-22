ee77057fd48737d935fffa6748b0b33a354c41b5
Eugene Bardosh <mr.jerr@gmail.com>
2016-09-22 22:15:01 +0300

    written script processing basr.json

--- bars.py
+++ bars.py
@@ -2,20 +2,45 @@ import json
 
 
 def load_data(filepath):
-    pass
+    with open('bars.json') as f:
+        json_data = json.load(f)
+    return json_data
 
 
 def get_biggest_bar(data):
-    pass
+    biggest = data[0]
+    for bar in data:
+        if bar['Cells']['SeatsCount'] > biggest['Cells']['SeatsCount']:
+            biggest = bar
+    return biggest
 
 
 def get_smallest_bar(data):
-    pass
+    smallest = data[0]
+    for bar in data:
+        if bar['Cells']['SeatsCount'] < smallest['Cells']['SeatsCount']:
+            smallest = bar
+    return smallest
 
 
 def get_closest_bar(data, longitude, latitude):
-    pass
+    def calc_distance(a,b):
+        return ((a[0]-b[0])**2+(a[1]-b[1])**2)**0.5
+    closest_bar = data[0]
+    distance_to_closest_bar = calc_distance((longitude, latitude), closest_bar['Cells']['geoData']['coordinates'])
+    for bar in data:
+        distance = calc_distance((longitude, latitude), bar['Cells']['geoData']['coordinates'])
+        if distance < distance_to_closest_bar:
+            distance_to_closest_bar = distance
+            closest_bar = bar
+    return closest_bar
 
 
 if __name__ == '__main__':
-    pass
+    json_data = load_data('bars.json')
+    print('The biggest bar\n{}'.format(get_biggest_bar(json_data)))
+    print('The smallest bar\n{}'.format(get_smallest_bar(json_data)))
+    longitude = float(input('Enter longitude\t:'))
+    latitude = float(input('Enter longitude\t:'))
+    print('The closest bar\n{}'.format(get_closest_bar(json_data, longitude, latitude)))
+
